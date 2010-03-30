9ac6626c99f10bca0b4b7afdd006f237f4d559b2
Wilker Lucio <wilkerlucio@gmail.com>
2010-03-30 14:23:13 -0300

    updated template

--- template.rb
+++ template.rb
@@ -4,14 +4,8 @@ repo = 'http://github.com/wilkerlucio/rails_template/raw/master'
 # ignoring files
 file ".gitignore", open("#{repo}/.gitignore").read
 
-# download pt-br i18n and configure environment
-file "config/locales/pt-BR.yml", open("#{repo}/pt-BR.yml").read
-gsub_file "config/environment.rb",
-          '# config.i18n.default_locale = :de', 'config.i18n.default_locale = "pt-BR"'
-
 # configure gems
-gem "authlogic"
-gem "searchlogic"
+gem "bistro_car"
 gem "mislav-will_paginate", :lib => 'will_paginate', :source => 'http://gems.github.com'
 gem "josevalim-inherited_resources", :lib => "inherited_resources", :source => "http://gems.github.com"
 gem "justinfrench-formtastic", :lib => 'formtastic', :source => 'http://gems.github.com'
@@ -22,18 +16,9 @@ gem "rspec", :lib => false
 gem "rspec-rails", :lib => false
 gem "webrat"
 
-# configure plugins
-plugin "validation_reflection", :git => "git://github.com/redinger/validation_reflection.git"
-
 # configure rspec and cucumber
 generate :rspec
 generate :cucumber
-
-# include webrat br steps
-file "features/step_definitions/webrat_steps_br.rb", open("#{repo}/webrat_steps_br.rb").read
-
-# configure gems manifest (for Heroku)
-file ".gems", open("#{repo}/.gems").read
 
 # create base populator
 rakefile "populate.rake", open("#{repo}/populate.rake").read
