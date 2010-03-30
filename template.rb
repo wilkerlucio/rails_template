ab0af4c73916cab6e2d73eb3c68f2df7c1e57827
Wilker Lucio <wilkerlucio@gmail.com>
2010-03-30 14:24:37 -0300

    updated jquery version

--- template.rb
+++ template.rb
@@ -27,7 +27,7 @@ rakefile "populate.rake", open("#{repo}/populate.rake").read
 run "rm public/javascripts/*"
 
 # add commonly used javascript libraries
-file "public/javascripts/jquery-1.4.1.min.js", open("http://code.jquery.com/jquery-1.4.1.min.js").read
+file "public/javascripts/jquery-1.4.2.min.js", open("http://code.jquery.com/jquery-1.4.2.min.js").read
 file "public/javascripts/jquery.autocomplete.js", open("#{repo}/javascripts/jquery.autocomplete.js").read
 file "public/javascripts/jquery.maskedinput-1.2.2.min.js", open("#{repo}/javascripts/jquery.maskedinput-1.2.2.min.js").read
 file "public/javascripts/application.js", open("#{repo}/javascripts/application.js").read
